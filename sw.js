90a5fe04bc5e1b2ee69d30319a95905d76f668fb
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-02 20:30:39 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'polish-dates-v1';
+const CACHE_NAME = 'polish-dates-v2.6';
 const ASSETS = [
   './',
   './index.html',
@@ -8,10 +8,29 @@ const ASSETS = [
   './icon.png'
 ];
 
+// Install: Cache all files
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Activate: Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Fetch: Serve from cache, then network
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
